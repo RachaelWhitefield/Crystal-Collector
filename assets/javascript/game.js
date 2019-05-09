b09521a8e47713038c8c0bfbcb6b9dec975f83e0
Fabulous_Rachael <rachael.whitefield@gmail.com>
2019-05-09 17:15:31 -0600

    game reset working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,5 @@
+
+
 $(document).ready(function () {
 
     var blue = $("#blue");
@@ -7,26 +9,78 @@ $(document).ready(function () {
     var pink = $("#pink");
     
     var yellow = $("#yellow");
+
+    var wins = document.getElementById("wins");
     
     
     var blueNumber = random1Thru12();
     blue.on("click", function () {
+        currentScore += blueNumber;
+        $("#current-score").text(currentScore);
+        if (currentScore > targetScore) {
+            console.log("Sorry, You Lose");
+            resetGame();
+            alert("Sorry, You Lose.");
+        }
+        
+        else if (currentScore === targetScore) {
+            resetGame();
+            alert("Yay! You Won!");
+        }
         console.log(blueNumber)
     })
     
     var greenNumber = random1Thru12();
     green.on("click", function () {
+        currentScore += greenNumber;
+        $("#current-score").text(currentScore);
+        if (currentScore > targetScore) {
+            console.log("Sorry, You Lose");
+            resetGame();
+            alert("Sorry, You Lose.");
+        }
+        
+        else if (currentScore === targetScore) {
+            resetGame();
+            alert("Yay! You Won!");
+        }
         console.log(greenNumber)
     })
 
     var pinkNumber = random1Thru12();
     pink.on("click", function () {
-        console.log(pinkNumber)
+        currentScore += pinkNumber;
+        $("#current-score").text(currentScore);
+        if (currentScore > targetScore) {
+            console.log("Sorry, You Lose");
+            resetGame();
+            alert("Sorry, You Lose.");
+        }
+        
+        else if (currentScore === targetScore) {
+            resetGame();
+            alert("Yay! You Won!");
+        }
+        console.log(pinkNumber);
     })
 
     var yellowNumber = random1Thru12();
     yellow.on("click", function () {
-        console.log(yellowNumber)
+        currentScore += yellowNumber;
+        $("#current-score").text(currentScore);
+        console.log("target score " + targetScore);
+        console.log("current-score " + currentScore);
+        if (currentScore > targetScore) {
+            console.log("Sorry, You Lose");
+            resetGame();
+            alert("Sorry, You Lose.");
+        }
+        
+        else if (currentScore === targetScore) {
+            resetGame();
+            alert("Yay! You Won!");
+        }
+            console.log(yellowNumber);
     })
 
    
@@ -40,14 +94,37 @@ $(document).ready(function () {
         return Math.floor(Math.random() * 120) + 19;
     }
 
-var yourScore = 0;
-var targetScore = 0;
+    var getRandom = function (min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+
+var currentScore = 0;
+var targetScore = getRandom(19, 120);
+$("#target-score").text(targetScore);
+console.log("This is the target score " + targetScore)
 
 var winCount = 0;
-var lossCount =0;
+var lossCount = 0;
 
 
 
+var resetGame = function() {
+    currentScore = 0;
+    $("#current-score").text(0);
+    targetScore = getRandom(19, 120);
+    yellowNumber = getRandom(1, 12);
+    pinkNumber = getRandom(1, 12);
+    greenNumber = getRandom(1, 12);
+    blueNumber = getRandom(1, 12);
+    $("#target-score").text(targetScore);
+
+    // crystal.blue.value = getRandom(1, 12);
+    // crystal.green.value = getRandom(1, 12);
+    // crystal.pink.value = getRandom(1, 12);
+    // crystal.yellow.value = getRandom(1, 12);
+
+    
+}
 
 
 
@@ -67,39 +144,5 @@ var lossCount =0;
 
 // Start/reset game
     // clear variables
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 })
